c785293a34efc14f5c412adc718ec5d9c0a93702
Nguyen Ngo <nqngo@ucdavis.edu>
2017-05-07 21:31:20 -0700

    Dockerfile for flask-video-streaming

---
+++ sample_apps/flask-video-streaming/README.md
@@ -0,0 +1,8 @@
+Dockerfile for flask-video-streaming
+
+build: docker build -t flask-video-streaming:latest .
+
+run: docker run -d -p 5000:5000 flask-video-streaming
+
+
+
--- server/server.py
+++ server/server.py
@@ -1,15 +1,15 @@
-import os
+import os, subprocess
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hello from Dockerized Flask App!!"
+  return "Hello from Dockerized Flask App!!"
 
 @app.route("/<application>")
 def runApplication(application):
-	os.system(application)
+  return subprocess.check_output("python " + application, shell=True)
 
 if __name__ == "__main__":
   app.run(debug=True,host='0.0.0.0')
